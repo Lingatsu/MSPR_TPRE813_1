26bc59e1fd2d4666fd2b53ad748c054a90f76faf
leote <leotep34@gmail.com>
2024-10-06 13:50:31 +0200

    mise a jour de docker pour permettre l'initialisation de la base de données et tout

--- scripts_sql/init.sql
+++ scripts_sql/init.sql
@@ -3,6 +3,15 @@ CREATE DATABASE IF NOT EXISTS mspr1;
 
 USE mspr1;
 
+CREATE TABLE IF NOT EXISTS communes (
+    dep TINYINT,
+    codecommune INT PRIMARY KEY,
+    nomdep VARCHAR(100),
+    nomcommune VARCHAR(100),
+    reg TINYINT,
+    nomreg VARCHAR(100)
+);
+
 CREATE TABLE IF NOT EXISTS faits (
     codecommune INT,
     annee INT,
@@ -16,13 +25,4 @@ CREATE TABLE IF NOT EXISTS faits (
     chomeurs INT,
     population INT,
     CONSTRAINT PK_Fait PRIMARY KEY (codecommune,annee)
-);
-
-CREATE TABLE IF NOT EXISTS communes (
-    dep TINYINT,
-    codecommune INT PRIMARY KEY,
-    nomdep VARCHAR(100),
-    nomcommune VARCHAR(100),
-    reg TINYINT,
-    nomreg VARCHAR(100)
 );
